3a94a7c16e9fdc1e4758005c534e6a46cbd3c11d
isaacm012502@gmail.com <isaacmcpherson@my.unt.edu>
2022-05-06 00:30:19 -0500

    updated getUserTurn function in Interface.cpp/h

--- interface.h
+++ interface.h
@@ -9,6 +9,6 @@ void displayBoard(Game c);
 void start(Game c);
 void play_two_users(Game c);
 void play_AI(Game c);
-void getUserTurn(int currentTurn);
+void getUserTurn(int currentPlayer);
 
 #endif
